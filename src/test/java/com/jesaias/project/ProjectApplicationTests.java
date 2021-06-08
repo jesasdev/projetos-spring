2149bb2de27f084a64ae7300acc97dd9b3c5e929
Jesaias Santos <jesasdev@gmail.com>
2021-06-07 21:29:42 -0300

    Projeto criado

---
+++ src/test/java/com/jesaias/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesaias.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
