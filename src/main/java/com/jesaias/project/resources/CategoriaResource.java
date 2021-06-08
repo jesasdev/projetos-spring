fdfa68c6480e09cd7d3ad27d32f9db42c50a943f
Jesaias Santos <jesasdev@gmail.com>
2021-06-07 22:33:03 -0300

    Primeira classe de domínio

--- src/main/java/com/jesaias/project/resources/CategoriaResource.java
+++ src/main/java/com/jesaias/project/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.jesaias.project.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jesaias.project.dominio.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return"O rest está funcionando";
+	public List<Categoria>listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informatica"); 
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		List<Categoria>list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 
 }
