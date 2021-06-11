a9c55194a94e73a08435f979120b9a53aa20880d
Jesaias Santos <jesasdev@gmail.com>
2021-06-10 21:59:44 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/jesaias/project/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.jesaias.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jesaias.project.dominio.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+
+}
